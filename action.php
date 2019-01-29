127c4668911f67938bd38075719c4cbbb285f5be
nanne <semicolon2@protonmail.com>
2019-01-29 14:39:35 +0530

    post

--- action.php
+++ action.php
@@ -1,6 +1,12 @@
 <?php
-	$name=$_GET["name"];
-	$email=$_GET["email"];
-	echo "Hello ".$name." How are you ?\n";
-	echo "We will contact you at ".$email;
+	$name=$_POST["name"];
+	$email=$_POST["email"];
+	$img=$_FILES["image"];
+	
+	//here were send data to the client
+	echo "data is not received successfully the received data is ".$name." and ".$email;
+	
+	//here we are uploading the file to the folder
+	$tmp="C:/xampp/htdocs/ajax/post/".$img["name"];
+	move_uploaded_file($img["tmp_name"],$tmp);
 ?>
--- index.js
+++ index.js
@@ -1,46 +1,53 @@
 window.onload=function(){
-	
-	//Getting the name and email from the user input
-	
-	var name=document.getElementById("name");
-	var email=document.getElementById("email");
-	var button=document.getElementById("submit");
-	
-	//when user click the send button
-	
-	button.addEventListener("click",function(){
-	
-		var http=new XMLHttpRequest();
-		http.open("GET","action.php?name="+name.value+"&email="+email.value,true);
-		http.send();
-		http.onreadystatechange=function(){
-			if(http.readyState==4&&http.status==200){
-				console.log(http.responseText);
-			}	
+	var button=document.getElementById("send");
+	send.onclick=function(){
+		var data=document.forms[0];
+		var fData=new FormData(data);
+		var xmlHttp=new XMLHttpRequest();
+		xmlHttp.open("POST","action.php");
+		xmlHttp.send(fData);
+		xmlHttp.onreadystatechange=function(){
+			if(xmlHttp.readyState==4&&xmlHttp.status==200){
+				console.log(xmlHttp.responseText);
+			}
 		}
-	});
+	}
 }
 
 /*
-	Syntax for the XMLHttpRequest;
-	var xmlhttp=new XMLHttpRequest();
-	xmlhttp.open(method,url,boolean);
-	here 
-	-> method is GET or POST
-	-> url is the url where the server side operations will be done
-	->	boolean is optional,true or false, true for async and false for sync if u didnt mention async
+	1.Here when the user click the send button the data will be send to the
+	  server using the ajax post method
+	2.The syntax for the ajax post method is
+		var xmlhttp=new XMLHttpRequest();
+		xmlHttp.open(method,url);
+		xmlHttp.send(data);
+		
+		method is post and the url is the file name of server
+		data is the values which we want to send to the server
 	
-	xmlhttp.send();
+	3.Here we use "FormData" api to collect all the inputs from the users
+	  then we send to server, it require form or data which is we can append to
+	  FormData api
+		ex:
+			<form method="POST">
+				name<input type="text" name="name"><br/>
+				email<input type="email" name="email"><br/>
+				<input type="button" value="send" id="send">
+			</form>
+			<script>
+				var button=document.getElementById("send");
+				send.onclick=function(){
+					var forms=document.forms[0];
+					var formData=new FormData(forms);
+					formData.forEach(function(data){
+						console.log(data);
+					});
+				};
+			</script>
 	
-	readyState
-		0 - request not initialized
-		1 - request has been set updateCommands
-		2 - request has been sent
-		3 - request is in process
-		4 - request is completed
-	
-	xmlhttp.responseText or xmlhttp.response
-	
-	The response data is consoled here we can do whatever 
-	operations we want with the response data
+	4.we can also append data to the FormData api use the append method 
+		syntax:
+			FormData.append(Name,Value);
+		ex:
+			formData.append("name","nanne");
 */
